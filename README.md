e3cd7edb2627a19d95c4fae0350cb429ab99629b
Anand Jain <anandj@uchicago.edu>
2024-08-29 21:02:58 -0700

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# fusion-scripts 
+
+the only useful script is the `disable all sketches` script.
+
+pro tip use the python intepreter at 
+and the python adsk code `C:\Users\anand\AppData\Local\Autodesk\webdeploy\production\1a197c6e79bef01edef1dc4f317d9f597820e633\Api\Python\packages\adsk `
+
+the .vscode folder sets it up so you get a bit of docstrings 
+
+todo see if i can find to better explore the object model 
+
+```C++
+#include <Core/CoreAll.h>
+#include <Fusion/FusionAll.h>
+#include <Cam/CamAll.h>
+```
---
+++ disable all sketches/disable all sketches.py
@@ -0,0 +1,27 @@
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def turn_off_sketch_visibility(component):
+    # Iterate through all sketches in the component and turn off visibility
+    for sketch in component.sketches:
+        sketch.isVisible = False
+
+    # Recursively handle all occurrences (nested components)
+    for occurrence in component.occurrences:
+        turn_off_sketch_visibility(occurrence.component)
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        design = app.activeProduct
+
+        # Start with the root component
+        root_comp = design.rootComponent
+        turn_off_sketch_visibility(root_comp)
+
+        ui.messageBox('All sketches are turned off.')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
